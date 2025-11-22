8ee665686e823eb045bbc24995eb86bab9bf262c
Ankita Das <daszankita23@gmail.com>
2025-11-22 22:47:13 +0530

    TICTACTOE

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import Board from './TicTacToeGame/Board';
+import logo from "./logo.svg";
+import "./App.css";
+import Board from "./TicTacToeGame/Board";
 function App() {
   return (
     <div className="App">
-      <Board/>
+      <Board />
     </div>
   );
 }
